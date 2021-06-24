023a2977e4a15faab40f08aa959afa5be7a1858d
GrzegorzNecka <strzypior@gmail.com>
2021-06-24 16:20:14 +0200

    1 file modified

--- index.js
+++ index.js
@@ -21,16 +21,26 @@ new Vue({
   },
   methods: {
     getInputVal: function(inputVal) {
-      // this.members = [...members];
-
-      console.log(inputVal);
+      if (!this.Validation(inputVal)) {
+        return;
+      }
 
       this.members.push({ name: inputVal });
-
       this.isValid = true;
     },
     resetInputVal: function() {
       return this.value;
+    },
+    Validation(inputVal) {
+      const isDuplicate = this.members.filter(member =>
+        member.name.includes(inputVal)
+      );
+
+      if (isDuplicate.length) {
+        alert(`parametr ${inputVal} już istnieje, podaj nowy paramter`);
+        return false;
+      }
+      return true;
     }
   }
 });
